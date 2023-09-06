50d4e47453aa3ed239e3c0b8f877c9fea65cfba0
Leo Martineau <leomartineau86@gmail.com>
2023-09-05 20:08:26 -0400

    debut TP2

---
+++ TP2/pb1/pb1.cpp
@@ -0,0 +1,20 @@
+/*
+ * Nom: compteur 32 bits
+ * Copyright (C) 2005 Matthew Khouzam
+ * License http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * Description: Ceci est un exemple simple de programme 
+ * Version: 1.1
+ * 
+ * Version 1.2 par Jérôme Collin, août 2023
+ * 
+ */
+
+#define F_CPU 8000000
+#include <util/delay.h>
+#include <avr/io.h> 
+
+int main()
+{
+  return 0; 
+}
+
